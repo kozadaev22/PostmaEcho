b53013c8c76592b528df9282df6419c7d498f6c0
kozadaev22 <kozadaev22@mail.ru>
2022-08-15 20:42:36 +0300

    Initial commit

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class PostmanEchoTest {
+    @Test
+    void shouldReturnBody() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Hi")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Hi"));
+    }
+}
